95d2c53b0102b718e771a895ddb834c909c83b0b
tasogarepg <tasogare.pg@gmail.com>
2012-03-11 14:13:15 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,144 @@
+# node-block
+
+An async contrl-flow library for Node.js.  
+Easily parallel execution and error handling.  
+
+## Installation
+
+    $ npm install node-block
+
+## Example
+
+```js
+var fs = require('fs');
+var block = require('node-block.js').block;
+
+block(
+  function() {
+    fs.readFile('/path/to/file1', 'utf8', this.async('d1'));
+    fs.readFile('/path/to/file2', 'utf8', this.async('d2'));
+  },
+  function() {
+    var str = this.data.d1 + this.data.d2;
+    console.log(str);
+  }
+)();
+```
+
+### Error handling
+
+Function name is `cat` and `fin`.
+
+```js
+var fs = require('fs');
+var block = require('node-block.js').block;
+
+block(
+  function() {
+    fs.readFile('/path/to/file1', 'utf8', this.async('d1'));
+    fs.readFile('/path/to/file2', 'utf8', this.async('d2'));
+  },
+  function() {
+    this.data.d3 = this.data.d1 + this.data.d2;
+  },
+  function cat(e) {       // catch
+    console.log(e);
+    throw e;
+  },
+  function fin() {        // finally
+    console.log('fin');   // always run
+  }
+)();
+```
+
+### Jump to end
+
+call `this.end()` with return.
+
+```js
+var fs = require('fs');
+var block = require('node-block.js').block;
+
+block(
+  function() {
+    if (true) {
+      return this.end();  // called with return
+    }
+  },
+  function() {
+    // not run here.
+  },
+  function cat(e) {       // catch
+    // when errorless, not run here.
+  },
+  function fin() {        // finally
+    console.log('fin');
+  }
+)();
+```
+
+### Callback
+
+sample() is called after fin().
+
+```js
+var fs = require('fs');
+var block = require('node-block.js').block;
+
+block(
+  function() {
+    fs.readFile('/path/to/file1', 'utf8', this.async('d1'));
+  },
+  function fin() {
+    console.log('fin');
+  }
+)(sample);
+
+function sample(err){
+  console.log(this.data.d1);
+}
+```
+
+### Nesting
+
+```js
+var fs = require('fs');
+var block = require('node-block.js').block;
+
+block(
+  function() {
+    fs.readFile('/path/to/file1', 'utf8', this.async('d1'));
+    fs.readFile('/path/to/file2', 'utf8', this.async('d2'));
+  },
+  function() {
+    fs.readFile('/path/to/file3', 'utf8', this.async('d3'));
+    block(
+      function() {
+        fs.readFile('/path/to/file4', 'utf8', this.async('e1'));
+        fs.readFile('/path/to/file5', 'utf8', this.async('e2'));
+      },
+      function() {
+        fs.readFile('/path/to/file6', 'utf8', this.async('e3'));
+      }
+    )(this.async('d4'));
+  },
+  function() {
+    var str = this.data.d1 + this.data.d2 + this.data.d3 +
+      this.data.d4.e1 + this.data.d4.e2 + this.data.d4.e3;
+    console.log(str);
+  }
+)();
+```
+
+## About API
+
+### this.async([String dataName])
+Make a callback for async function. [String dataName] is name of setting result data. It's named arbitrarily.  
+
+### this.end([Error err])
+Jump to end. If [Error err] is set jump to cat(), else jump to fin() or callback. If cat(), fin(), callback don't exist , exit block.  
+This api must be called with 'return'.  
+
+## License
+
+The MIT License
---
+++ test/node-block.test.js
@@ -0,0 +1,381 @@
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+var block = require('../lib/node-block.js').block;
+
+var fileA = path.join(__dirname, 'rsc', '_a.txt'); // aaaa
+var fileB = path.join(__dirname, 'rsc', '_b.txt'); // bbbb
+
+describe('node-block', function() {
+
+  it('sync chain', function(done) {
+    block(
+      function() {
+        this.data.d1 = 'a';
+      },
+      function() {
+        this.data.d1 += 'b';
+      },
+      function() {
+        assert.equal(this.data.d1, 'ab');
+      }
+    )(done);
+  });
+
+  it('async chain serial', function(done) {
+    block(
+      function() {
+        fs.readFile(fileA, 'utf8', this.async('d1'));
+      },
+      function() {
+        fs.readFile(fileB, 'utf8', this.async('d2'));
+      },
+      function() {
+        var str = this.data.d1 + this.data.d2;
+        assert.equal(str, 'aaaabbbb');
+      }
+    )(done);
+  });
+
+  it('async chain parallel', function(done) {
+    block(
+      function() {
+        fs.readFile(fileA, 'utf8', this.async('d1'));
+        fs.readFile(fileB, 'utf8', this.async('d2'));
+      },
+      function() {
+        fs.readFile(fileA, 'utf8', this.async('d3'));
+        fs.readFile(fileB, 'utf8', this.async('d4'));
+      },
+      function() {
+        var str = this.data.d1 + this.data.d2 + this.data.d3 + this.data.d4;
+        assert.equal(str, 'aaaabbbbaaaabbbb');
+      }
+    )(done);
+  });
+
+  it('async parallel set time', function(done) {
+    block(
+      function() {
+        this.data.d1 = '';
+        var that = this;
+        var cb1 = this.async();
+        setTimeout(function() {
+          that.data.d1 += 'a';
+          cb1();
+        }, 40);
+        var cb2 = this.async();
+        setTimeout(function() {
+          that.data.d1 += 'b';
+          cb2();
+        }, 20);
+        var cb3 = this.async();
+        setTimeout(function() {
+          that.data.d1 += 'c';
+          cb3();
+        }, 1);
+      },
+      function() {
+        assert.equal(this.data.d1, 'cba');
+      }
+    )(done);
+  });
+
+  it('async chain no delay', function(done) {
+    block(
+      function() {
+        this.async('d1')(null, 'a');
+      },
+      function() {
+        this.async('d2')(null, 'b');
+      },
+      function() {
+        var str = this.data.d1 + this.data.d2;
+        assert.equal(str, 'ab');
+      }
+    )(done);
+  });
+
+  it('no exception', function(done) {
+    block(
+      function() {
+        this.data.d1 = 'a';
+      },
+      function() {
+        this.data.d1 += 'b';
+      },
+      function cat(err) {
+        this.data.d1 += 'c';
+      },
+      function fin() {
+        assert.equal(this.data.d1, 'ab');
+      }
+    )(done);
+  });
+
+  it('throw exception', function(done) {
+    block(
+      function() {
+        throw new Error('test');
+      },
+      function() {
+        assert(false);
+      },
+      function cat(err) {
+        assert.equal(err.message, 'test');
+      }
+    )(done);
+  });
+
+  it('throw exception from async', function(done) {
+    block(
+      function() {
+        fs.readFile('not_found', 'utf8', this.async('d1'));
+      },
+      function() {
+        assert(false, 'assert');
+      },
+      function cat(err) {
+        assert.notEqual(err.message, 'assert');
+        assert.notEqual(err, null);
+      }
+    )(done);
+  });
+
+  it('throw exception from async parallel', function(done) {
+    block(
+      function() {
+        fs.readFile('not_found', 'utf8', this.async('d1'));
+        fs.readFile('not_found', 'utf8', this.async('d2'));
+        fs.readFile('not_found', 'utf8', this.async('d3'));
+      },
+      function() {
+        assert(false, 'assert');
+      },
+      function cat(err) {
+        assert.notEqual(err.message, 'assert');
+        assert.notEqual(err, null);
+      }
+    )(done);
+  });
+
+  it('throw exception from inner block', function(done) {
+    block(
+      function() {
+        block(
+          function() {
+            ;
+          },
+          function() {
+            throw new Error('test');
+          },
+          function() {
+            assert(false);
+          }
+        )(this.async());
+      },
+      function() {
+        assert(false);
+      },
+      function cat(err) {
+        assert.equal(err.message, 'test');
+      }
+    )(done);
+  });
+
+  it('catch exception and throw', function(done) {
+    block(
+      function() {
+        throw new Error('test');
+      },
+      function() {
+        assert(false);
+      },
+      function cat(err) {
+        assert.notEqual(err, null);
+        assert.equal(err.message, 'test');
+        throw err;
+      },
+      function fin(err) {
+        assert.notEqual(err, null);
+        assert.equal(err.message, 'test');
+      }
+    )(function(err) {
+      assert.notEqual(err, null);
+      done((err.message == 'test') ? null : err);
+    });
+  });
+
+  it('no catch exception', function(done) {
+    block(
+      function() {
+        throw new Error('test');
+      },
+      function() {
+        assert(false);
+      }
+    )(function(err) {
+      assert.notEqual(err, null);
+      done((err.message == 'test') ? null : err);
+    });
+  });
+
+  it('no catch exception and fin', function(done) {
+    block(
+      function() {
+        throw new Error('test');
+      },
+      function() {
+        assert(false);
+      },
+      function fin(err) {
+        assert.notEqual(err, null);
+        assert.equal(err.message, 'test');
+      }
+    )(function(err) {
+      assert.notEqual(err, null);
+      done((err.message == 'test') ? null : err);
+    });
+  });
+
+  it('no catch exception and fin async', function(done) {
+    block(
+      function() {
+        throw new Error('test');
+      },
+      function() {
+        assert(false);
+      },
+      function fin(err) {
+        assert.notEqual(err, null);
+        assert.equal(err.message, 'test');
+        fs.readFile(fileA, 'utf8', this.async('d1'));
+        fs.readFile(fileB, 'utf8', this.async('d2'));
+      }
+    )(function(err) {
+      var str = this.data.d1 + this.data.d2;
+      assert.equal(str, 'aaaabbbb');
+      assert.notEqual(err, null);
+      done((err.message == 'test') ? null : err);
+    });
+  });
+
+  it('jump to end', function(done) {
+    block(
+      function() {
+        this.data.d1 = 'a';
+        if (true) {
+          return this.end();
+        }
+        assert(false);
+      },
+      function() {
+        assert(false);
+      },
+      function cat(err) {
+        assert.notEqual(err, null);
+        throw err;
+      },
+      function fin(err) {
+        assert.equal(err, null);
+        this.data.d1 += 'b';
+      }
+    )(function(err) {
+      assert.equal(this.data.d1, 'ab');
+      done(err);
+    });
+  });
+
+  it('nested block', function(done) {
+    block(
+      function() {
+        fs.readFile(fileA, 'utf8', this.async('d1'));
+        fs.readFile(fileB, 'utf8', this.async('d2'));
+      },
+      function() {
+        fs.readFile(fileA, 'utf8', this.async('d3'));
+        block(
+          function() {
+            fs.readFile(fileB, 'utf8', this.async('e1'));
+            fs.readFile(fileA, 'utf8', this.async('e2'));
+          },
+          function() {
+            fs.readFile(fileB, 'utf8', this.async('e3'));
+          }
+        )(this.async('d4'));
+      },
+      function() {
+        var str = this.data.d1 + this.data.d2 + this.data.d3 +
+          this.data.d4.e1 + this.data.d4.e2 + this.data.d4.e3;
+        assert.equal(str, 'aaaabbbbaaaabbbbaaaabbbb');
+      }
+    )(done);
+  });
+
+  it('nested and parallel block', function(done) {
+    block(
+      function() {
+        fs.readFile(fileA, 'utf8', this.async('d1'));
+        fs.readFile(fileB, 'utf8', this.async('d2'));
+      },
+      function() {
+        fs.readFile(fileA, 'utf8', this.async('d3'));
+        block(
+          function() {
+            fs.readFile(fileB, 'utf8', this.async('e1'));
+            fs.readFile(fileA, 'utf8', this.async('e2'));
+          },
+          function() {
+            fs.readFile(fileB, 'utf8', this.async('e3'));
+          }
+        )(this.async('d4'));
+        block(
+          function() {
+            fs.readFile(fileA, 'utf8', this.async('e1'));
+          },
+          function() {
+            fs.readFile(fileB, 'utf8', this.async('e2'));
+            fs.readFile(fileA, 'utf8', this.async('e3'));
+          }
+        )(this.async('d5'));
+      },
+      function() {
+        var str = this.data.d1 + this.data.d2 + this.data.d3 +
+          this.data.d4.e1 + this.data.d4.e2 + this.data.d4.e3 +
+          this.data.d5.e1 + this.data.d5.e2 + this.data.d5.e3;
+        assert.equal(str, 'aaaabbbbaaaabbbbaaaabbbbaaaabbbbaaaa');
+      }
+    )(done);
+  });
+
+  it('bench', function(done) {
+    var loopCount = 10000;
+    (function serialLoop() {
+      if (loopCount-- <= 0) {
+        done();
+        return;
+      }
+      block(
+        function() {
+          process.nextTick(this.async());
+        },
+        function() {
+          process.nextTick(this.async());
+          process.nextTick(this.async());
+        },
+        function() {
+          process.nextTick(this.async());
+          process.nextTick(this.async());
+          process.nextTick(this.async());
+        },
+        function() {
+          process.nextTick(this.async());
+          process.nextTick(this.async());
+          process.nextTick(this.async());
+          process.nextTick(this.async());
+        }
+      )(serialLoop);
+    })();
+  });
+
+});
